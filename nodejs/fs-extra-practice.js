7d750dc1ee507a6b6079790572d84ca3c20dd3d3
alex ramanouskaya <alexramanouskaya@gmail.com>
2026-02-03 20:30:15 +0300

    changed path to files

--- nodejs/fs-extra-practice.js
+++ nodejs/fs-extra-practice.js
@@ -1,52 +1,52 @@
 const fs = require('fs-extra');
 
 function createFirstFolder(){
-const folderPath = 'C:/Users/acer/aqa-js-course/nodejs/folder1';
+const folderPath = './nodejs/folder1';
 fs.ensureDirSync(folderPath)
 }
 
 function createFistTextFile(){
-const file = 'C:/Users/acer/aqa-js-course/nodejs/folder1/textfile.txt';
+const file = './nodejs/folder1/textfile.txt';
 fs.outputFileSync(file, 'God help me')   
 }
 
 function createSecondFolder(){
-const folder2Path = 'C:/Users/acer/aqa-js-course/nodejs/folder2'
+const folder2Path = './nodejs/folder2'
 fs.ensureDirSync(folder2Path)
 }
 
 function moveTextFile (){
-const file1Path = 'C:/Users/acer/aqa-js-course/nodejs/folder1/textfile.txt';
-const file2Path = 'C:/Users/acer/aqa-js-course/nodejs/folder2/textfile.txt';
+const file1Path = './nodejs/folder1/textfile.txt';
+const file2Path = './nodejs/folder2/textfile.txt';
 fs.moveSync(file1Path, file2Path)
 }
 
 function createThirdFolder(){
-const folderPath = 'C:/Users/acer/aqa-js-course/nodejs/folder3';
+const folderPath = './nodejs/folder3';
 fs.ensureDirSync(folderPath)
 }
 
 function copyFile(){
-const file2Path = 'C:/Users/acer/aqa-js-course/nodejs/folder2/textfile.txt';
-const file3Path = 'C:/Users/acer/aqa-js-course/nodejs/folder3/copyfile.txt';
+const file2Path = './nodejs/folder2/textfile.txt';
+const file3Path = './nodejs/folder3/copyfile.txt';
 fs.copySync(file2Path, file3Path);
 }
 
-// function deleteFiles(){
-// const remove1stFile = 'C:/Users/acer/aqa-js-course/nodejs/folder2/textfile.txt';
-// const remove2ndFile = 'C:/Users/acer/aqa-js-course/nodejs/folder3/copyfile.txt';
-// fs.removeSync(remove1stFile);
-// fs.removeSync(remove2ndFile);
-// }
+function deleteFiles(){
+const remove1stFile = './nodejs/folder2/textfile.txt';
+const remove2ndFile = './nodejs/folder3/copyfile.txt';
+fs.removeSync(remove1stFile);
+fs.removeSync(remove2ndFile);
+}
 
-// function deleteFolders(){
-// const remove1stFolder = 'C:/Users/acer/aqa-js-course/nodejs/folder1';
-// const remove2ndFolder = 'C:/Users/acer/aqa-js-course/nodejs/folder2';
-// const remove3rdFolder = 'C:/Users/acer/aqa-js-course/nodejs/folder3';
-// fs.removeSync(remove1stFolder);
-// fs.removeSync(remove2ndFolder);
-// fs.removeSync(remove3rdFolder);
-// }
+function deleteFolders(){
+const remove1stFolder = './nodejs/folder1';
+const remove2ndFolder = './nodejs/folder2';
+const remove3rdFolder = './nodejs/folder3';
+fs.removeSync(remove1stFolder);
+fs.removeSync(remove2ndFolder);
+fs.removeSync(remove3rdFolder);
+}
 
 createFirstFolder()
 createFistTextFile()
