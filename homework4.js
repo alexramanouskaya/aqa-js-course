a1001ee802da53543aeab327162b36dc8f54039d
alex ramanouskaya <alexramanouskaya@gmail.com>
2026-02-15 16:41:13 +0300

    rename

--- homework4.js
+++ homework4.js
@@ -1,29 +1,22 @@
-// ; 1. поменять массив в обратном порядке - [1,2,3,4,5,6] [6,5,4,3,2,1]
-// ; 2. найти максимальное значение числа в массиве ([3,67,15...])
-// ; 3. записать в массив ряд фибоначчи начиная с N члена с длинной массива M
-// ; 4. даны 2 4-х значных числа с неповторяющимися цифрами, надо определить сколько цифр в этих числах совпадают по значению и позиции и сколько только по значению, но не по позиции (3487 и 3794 ---> 1 и 2 )
-// ; 5. сортировка массива по возрастанию/убыванию
-// ; 6. удалить из массива все повторяющиеся элементы
-
 
 
 // 1.поменять массив в обратном порядке - [1,2,3,4,5,6] [6,5,4,3,2,1]
-const massive1 = [1,2,3,4,5,6] 
-const reversedMassive = massive1.reverse()
+const array1 = [1,2,3,4,5,6] 
+const reversedMassive = array1.reverse()
 console.log(reversedMassive)
 
 //2-ой способ 
-reversedMassive2 = [] 
-for (let i = 0 ; i < massive1.length ; i++) {
-reversedMassive2.push(massive1[i])
+reversedArray2 = [] 
+for (let i = 0 ; i < array1.length ; i++) {
+reversedArray2.push(array1[i])
 }
-console.log('Reversed Massive 2', reversedMassive2)
+console.log('Reversed Massive 2', reversedArray2)
 
 
 // 2. найти максимальное значение числа в массиве ([3,67,15...])
 
-const massive2 = [3, 67, 15, 127, 144]
-const maxNumber = Math.max (...massive2)
+const array2 = [3, 67, 15, 127, 144]
+const maxNumber = Math.max (...array2)
 console.log(maxNumber)
 
 // 3. записать в массив ряд фибоначчи начиная с N члена с длинной массива M
@@ -47,16 +40,16 @@ func1(5, 9) ;
 //const massive4 = [3487 , 3794 ]
 let number1 = 3487
 let number2 = 3794
-let massiveNumber1 = number1.toString().split('').map(Number)
-let massiveNumber2 = number2.toString().split('').map(Number)
+let arrayNumber1 = number1.toString().split('').map(Number)
+let arrayNumber2 = number2.toString().split('').map(Number)
 let digit = 0
 let positionDigit = 0
-massiveNumber1.forEach((number , index) => {
-    if (number === massiveNumber2[index])
+arrayNumber1.forEach((number , index) => {
+    if (number === arrayNumber2[index])
     {
         positionDigit++
     }
-    else if (massiveNumber2.includes(number))
+    else if (arrayNumber2.includes(number))
     {
       digit++
     }
@@ -68,26 +61,26 @@ console.log("Совпадений по числу - ", digit)
 
 // 5. сортировка массива по возрастанию/убыванию
 
-const massive5 = [1, 88, 63, 901, 123, 12, 44] ;
-function sortMassive5(a, b) {
+const array5 = [1, 88, 63, 901, 123, 12, 44] ;
+function sortArray5(a, b) {
   return a - b ;
 }
-massive5.sort(sortMassive5) ; 
-console.log(massive5)
+array5.sort(sortArray5) ; 
+console.log(array5)
 
-const massive6 = [11, 78, 2, 999, 34, 9, 532]
-function sort2Massive (a,b) {
+const array6 = [11, 78, 2, 999, 34, 9, 532]
+function sort2Array (a,b) {
   return b - a ; 
 }
-massive6.sort(sort2Massive) ;
-console.log(massive6)
+array6.sort(sort2Array) ;
+console.log(array6)
 
 //6. удалить из массива все повторяющиеся элементы
-const massive7 = [33, 11, 45, 177, 33, 67, 11]
+const array7 = [33, 11, 45, 177, 33, 67, 11]
 const noDuplicates = [];
-for (let i = 0; i < massive7.length; i++) {
-    if (noDuplicates.indexOf(massive7[i]) === -1) { 
-        noDuplicates.push(massive7[i]);
+for (let i = 0; i < array7.length; i++) {
+    if (noDuplicates.indexOf(array7[i]) === -1) { 
+        noDuplicates.push(array7[i]);
     }
 }
 console.log(noDuplicates)
